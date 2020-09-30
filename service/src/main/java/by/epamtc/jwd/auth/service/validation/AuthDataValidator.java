510bb353c1c0b0f042298be37af8027c790867cc
Siarhei Busel <alexanaid@tut.by>
2020-09-30 19:53:18 +0300

    Correct AuthDataValidator logically

--- service/src/main/java/by/epamtc/jwd/auth/service/validation/AuthDataValidator.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/validation/AuthDataValidator.java
@@ -6,25 +6,25 @@ import by.epamtc.jwd.auth.service.exception.PasswordValidationServiceException;
 import by.epamtc.jwd.auth.service.exception.ValidationServiceException;
 
 public class AuthDataValidator {
-    public boolean validateLogin(String login) {
+    public boolean isLoginValid(String login) {
         return (login.length() >= 3) && (login.length() <= 20)
                 && login.matches("[A-Za-z.-_0-9]+");
     }
 
-    public boolean validatePassword(String password) {
+    public boolean isPasswordValid(String password) {
         return (password.length() >= 6)
                 && password.matches("[A-Za-zА-ЯаяЁё0-9!?@#$%^&*()\\-_+:;,.\"']+");
     }
 
     public boolean isAuthDataValid(String login, String password)
             throws ValidationServiceException {
-        if (validateLogin(login)) {
-            if (validatePassword(password)) {
+        if (!isLoginValid(login)) {
+            if (!isPasswordValid(password)) {
                 throw new AuthDataValidationServiceException();
             }
             throw new LoginValidationServiceException();
         }
-        if (validatePassword(password)) {
+        if (!isPasswordValid(password)) {
             throw new PasswordValidationServiceException();
         }
         return true;
