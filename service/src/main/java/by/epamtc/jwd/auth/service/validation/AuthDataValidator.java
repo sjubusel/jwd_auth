e6c56fb1cdc6bcffb313b8fe0546e76ac58c3a58
Siarhei Busel <alexanaid@tut.by>
2020-09-30 13:46:56 +0300

    Add a full auth-data validation on a service-layer

--- service/src/main/java/by/epamtc/jwd/auth/service/validation/AuthDataValidator.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/validation/AuthDataValidator.java
@@ -7,11 +7,13 @@ import by.epamtc.jwd.auth.service.exception.ValidationServiceException;
 
 public class AuthDataValidator {
     public boolean validateLogin(String login) {
-        return false;
+        return (login.length() >= 3) && (login.length() <= 20)
+                && login.matches("[A-Za-z.-_0-9]+");
     }
 
     public boolean validatePassword(String password) {
-        return false;
+        return (password.length() >= 6)
+                && password.matches("[A-Za-zА-ЯаяЁё0-9!?@#$%^&*()\\-_+:;,.\"']+");
     }
 
     public boolean isAuthDataValid(String login, String password)
