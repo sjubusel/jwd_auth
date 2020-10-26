2d5d2b72d9226f80787e92814daf2f1af8498b0e
Siarhei Busel <sju.busel@gmail.com>
2020-10-27 00:42:16 +0300

    Refactor register()-method in DefaultAuthUserService

--- service/src/main/java/by/epamtc/jwd/auth/service/impl/DefaultAuthUserService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/impl/DefaultAuthUserService.java
@@ -4,20 +4,21 @@ import by.epamtc.jwd.auth.dao.AuthUserDao;
 import by.epamtc.jwd.auth.dao.DaoFactory;
 import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
-import by.epamtc.jwd.auth.model.auth_info.Role;
+import by.epamtc.jwd.auth.model.auth_info.RegistrationInfo;
 import by.epamtc.jwd.auth.service.AuthUserService;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
-import by.epamtc.jwd.auth.service.validation.AuthDataValidator;
-
-import java.nio.charset.StandardCharsets;
+import by.epamtc.jwd.auth.service.validation.RegistrationInfoValidator;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class DefaultAuthUserService implements AuthUserService {
     private DaoFactory daoFactory = DaoFactory.getInstance();
     private AuthUserDao authUserDao = daoFactory.getAuthUserDao();
-    private AuthDataValidator authDataValidator = new AuthDataValidator();
+    private RegistrationInfoValidator regInfValidator
+            = new RegistrationInfoValidator();
 
     @Override
-    public AuthUser login(String login, byte[] password) throws ServiceException {
+    public AuthUser login(String login, byte[] password)
+            throws ServiceException {
         AuthUser user;
         try {
             user = authUserDao.receiveAuthUserIfCorrect(login, password);
@@ -30,29 +31,39 @@ public class DefaultAuthUserService implements AuthUserService {
     }
 
     @Override
-    public AuthUser register(String login, String password) throws ServiceException {
-        byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
-        if (authDataValidator.isAuthDataValidOtherwiseThrow(login, password)) {
+    public AuthUser register(RegistrationInfo regInfo)
+            throws ServiceException {
+        if (regInfValidator.isRegistrationInfoValid(regInfo)) {
             try {
-                if (!authUserDao.containsLogin(login)) {
-                    authUserDao.saveAuthUser(new AuthUser(login, password,
-                            Role.USER));
-                    return authUserDao.receiveAuthUserIfCorrect(login,
-                            passwordBytes);
+                if (!authUserDao.containsLogin(regInfo.getLogin())
+                        && !authUserDao.containsEmail(regInfo.getEmail())) {
+                    hashPassword(regInfo);
+                    return authUserDao.saveAuthUser(regInfo);
                 }
             } catch (DaoException e) {
                 throw new ServiceException(e);
             } finally {
-                cleanOutPassword(passwordBytes);
+                cleanOutPassword(regInfo);
             }
         }
-
         return null;
     }
 
+    private void hashPassword(RegistrationInfo regInfo) {
+        String salt = BCrypt.gensalt();
+        String hashedPassword = BCrypt.hashpw(regInfo.getPassword(), salt);
+        regInfo.setPassword(hashedPassword);
+    }
+
+    private void cleanOutPassword(RegistrationInfo registrationInfo) {
+        registrationInfo.setPassword(null);
+    }
+
     private void cleanOutPassword(byte[] passwordBytes) {
+        // TODO ??? delete ???
         for (int i = 0; i < passwordBytes.length; i++) {
             passwordBytes[i] = 0;
         }
     }
+
 }
