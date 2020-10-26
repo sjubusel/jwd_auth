7be1ae2212ce183b008cbc2ae03873ba9a012cee
Siarhei Busel <sju.busel@gmail.com>
2020-10-26 12:48:19 +0300

    Add a UtilConstant class, which is to contain utility Strings

---
+++ model/src/main/java/by/epamtc/jwd/auth/model/constant/UtilConstant.java
@@ -0,0 +1,10 @@
+package by.epamtc.jwd.auth.model.constant;
+
+public final class UtilConstant {
+    public static final String ONE_WHITESPACE = " ";
+    public static final String OPENING_PARENTHESIS = "(";
+    public static final String CLOSING_PARENTHESIS = ")";
+
+    private UtilConstant() {
+    }
+}
