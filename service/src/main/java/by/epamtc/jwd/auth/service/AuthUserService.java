5163e3adab1d23c174d76051018d9973bf20bdbe
Siarhei Busel <sju.busel@gmail.com>
2020-10-26 20:24:54 +0300

    Change a contract of register()-method in AuthUserService

--- service/src/main/java/by/epamtc/jwd/auth/service/AuthUserService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/AuthUserService.java
@@ -1,11 +1,11 @@
 package by.epamtc.jwd.auth.service;
 
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+import by.epamtc.jwd.auth.model.auth_info.RegistrationInfo;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
 
 public interface AuthUserService {
     AuthUser login(String login, byte[] password) throws ServiceException;
 
-    AuthUser register(String login, String password) throws ServiceException;
-
+    AuthUser register(RegistrationInfo registrationInfo) throws ServiceException;
 }
