ef7a4889aac0c0392a27a855ae3454dea90501ec
Siarhei Busel <sju.busel@gmail.com>
2020-10-28 18:58:48 +0300

    Rewrite RegisterCommand

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/RegisterCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/RegisterCommand.java
@@ -2,11 +2,9 @@ package by.epamtc.jwd.auth.web.util.impl;
 
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.auth_info.RegistrationInfo;
+import by.epamtc.jwd.auth.model.constant.AppConstant;
 import by.epamtc.jwd.auth.service.AuthUserService;
 import by.epamtc.jwd.auth.service.ServiceFactory;
-import by.epamtc.jwd.auth.service.exception.AuthDataValidationServiceException;
-import by.epamtc.jwd.auth.service.exception.LoginValidationServiceException;
-import by.epamtc.jwd.auth.service.exception.PasswordValidationServiceException;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.util.Command;
 import by.epamtc.jwd.auth.model.constant.AppAttribute;
@@ -31,31 +29,58 @@ public class RegisterCommand implements Command {
 
         try {
             user = authUserService.register(regInfo);
-        } catch (AuthDataValidationServiceException e) {
-            res.sendRedirect(req.getContextPath()
-                    + CommandPath.REGISTER_AUTH_ERROR);
-            return;
-        } catch (LoginValidationServiceException e) {
-            res.sendRedirect(req.getContextPath()
-                    + CommandPath.REGISTER_LOGIN_ERROR);
-            return;
-        } catch (PasswordValidationServiceException e) {
-            res.sendRedirect(req.getContextPath()
-                    + CommandPath.REGISTER_PASSWORD_ERROR);
-            return;
         } catch (ServiceException e) {
-            res.sendRedirect(req.getContextPath()
-                    + CommandPath.REGISTER_TECH_ERROR);
+            // TODO log4j in catch of ServiceException
+            res.sendRedirect(req.getContextPath() + CommandPath
+                    .REGISTER_TECH_ERROR);
             return;
         }
 
-        if (user != null) {
-            req.getSession().setAttribute(AppAttribute.SESSION_AUTH_DATA, user);
-            res.sendRedirect(req.getContextPath() + CommandPath.PROFILE_GET);
+        if (user == null) {
+            sendRedirectWithValidationError(req, res);
+            return;
+        }
+
+        if (doesUserWithTheseCredentialsExist(user)) {
+            sendRedirectWithDuplicateError(user, req, res);
+            return;
+        }
+
+        req.getSession().setAttribute(AppAttribute.SESSION_AUTH_DATA, user);
+        res.sendRedirect(req.getContextPath() + CommandPath.PROFILE_GET);
+    }
+
+    private void sendRedirectWithValidationError(HttpServletRequest req,
+            HttpServletResponse res) throws IOException {
+        res.sendRedirect(req.getContextPath() + CommandPath
+                .REG_INFO_VAL_ERROR);
+    }
+
+    private void sendRedirectWithDuplicateError(AuthUser user,
+            HttpServletRequest req, HttpServletResponse res) throws IOException {
+        if (doesUserWithBothTheseLoginAndEmailExist(user)) {
+            res.sendRedirect(req.getContextPath() + CommandPath
+                    .REGISTER_DUPLICATE_ERROR);
+        } else if (doesUserWithOnlyThisLoginExist(user)) {
+            res.sendRedirect(req.getContextPath() + CommandPath
+                    .REGISTER_DUPLICATE_ERROR_BY_LOGIN);
         } else {
-            res.sendRedirect(req.getContextPath()
-                    + CommandPath.REGISTER_DUPLICATE_ERROR);
+            res.sendRedirect(req.getContextPath() + CommandPath
+                    .REGISTER_DUPLICATE_ERROR_BY_EMAIL);
         }
     }
-}
 
+    private boolean doesUserWithTheseCredentialsExist(AuthUser user) {
+        return user.getId() == AppConstant.DUPLICATE_AUTH_USER_LOGIN_ID
+                || user.getUserId() == AppConstant.DUPLICATE_AUTH_USER_EMAIL_ID;
+    }
+
+    private boolean doesUserWithBothTheseLoginAndEmailExist(AuthUser user) {
+        return user.getId() == AppConstant.DUPLICATE_AUTH_USER_LOGIN_ID
+                && user.getUserId() == AppConstant.DUPLICATE_AUTH_USER_EMAIL_ID;
+    }
+
+    private boolean doesUserWithOnlyThisLoginExist(AuthUser user) {
+        return user.getUserId() == AppConstant.DUPLICATE_AUTH_USER_LOGIN_ID;
+    }
+}
