3f5687025db366aac7bff2ab3937f011f80c3ff5
Siarhei Busel <sju.busel@gmail.com>
2020-10-29 01:14:51 +0300

    Refactor AuthUserDao

--- dao/src/main/java/by/epamtc/jwd/auth/dao/AuthUserDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/AuthUserDao.java
@@ -2,10 +2,11 @@ package by.epamtc.jwd.auth.dao;
 
 import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+import by.epamtc.jwd.auth.model.auth_info.AuthenticationInfo;
 import by.epamtc.jwd.auth.model.auth_info.RegistrationInfo;
 
 public interface AuthUserDao {
-    AuthUser receiveAuthUserIfCorrect(String login, byte[] password)
+    AuthUser receiveAuthUserIfCorrect(AuthenticationInfo authenticationInfo)
             throws DaoException;
 
     AuthUser saveAuthUser(RegistrationInfo registrationInfo) throws DaoException;
