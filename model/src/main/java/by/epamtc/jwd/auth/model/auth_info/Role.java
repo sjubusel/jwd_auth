124faad607556cfa69c5c544f3b4a51b9724223c
Siarhei Busel <sju.busel@gmail.com>
2020-10-28 15:15:24 +0300

    Refactor a Role-enum

--- model/src/main/java/by/epamtc/jwd/auth/model/auth_info/Role.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/auth_info/Role.java
@@ -1,5 +1,15 @@
 package by.epamtc.jwd.auth.model.auth_info;
 
 public enum Role {
-    USER
+    USER(1);
+
+    private int roleId;
+
+    Role(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
 }
