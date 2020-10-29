469c42b8dbd4ed25d66c86d18d3189c79a823e17
Siarhei Busel <sju.busel@gmail.com>
2020-10-29 23:14:19 +0300

    Refactor DefaultAuthUserDao

--- dao/src/main/java/by/epamtc/jwd/auth/dao/impl/DefaultAuthUserDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/impl/DefaultAuthUserDao.java
@@ -18,8 +18,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DefaultAuthUserDao implements AuthUserDao {
     private ConnectionPool pool = ConnectionPool.getInstance();
@@ -34,7 +32,7 @@ public class DefaultAuthUserDao implements AuthUserDao {
         try {
             conn = pool.takeConnection();
             selectAuthUserFromDataBaseQuery = conn
-                    .prepareStatement(SqlStatement.SELECT_AUTH_USER);
+                    .prepareStatement(SqlStatement.SELECT_AUTH_USER_BY_LOGIN);
             selectAuthUserFromDataBaseQuery.setString(1, authInfo.getLogin());
             rSet = selectAuthUserFromDataBaseQuery.executeQuery();
             if (rSet.next()) {
@@ -57,108 +55,52 @@ public class DefaultAuthUserDao implements AuthUserDao {
     }
 
     @Override
-    public AuthUser saveAuthUser(RegistrationInfo registrationInfo) throws DaoException {
+    public AuthUser saveAuthUser(RegistrationInfo regInfo) throws DaoException {
         Connection conn = null;
-        // TODO make an array of PreparedStatements
-        List<PreparedStatement> statements = new ArrayList<>(2);
-//        PreparedStatement stat = null;
+        PreparedStatement[] statements = new PreparedStatement[3];
+        ResultSet rSet = null;
+        AuthUser authUser = null;
+        int statIndex = 0;
 
         try {
             conn = pool.takeConnection();
             conn.setAutoCommit(false);
-            // TODO replace queries with constants
-            PreparedStatement personCreationStat = conn.prepareStatement(
-                    "INSERT INTO hospital.persons (email, phone_number, first_name, middle_name, last_name, birth_date, gender) VALUES (?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-            personCreationStat.setString(1, registrationInfo.getEmail());
-            personCreationStat.setString(2, registrationInfo.getPhoneNumber());
-            personCreationStat.setString(3, registrationInfo.getFirstName());
-            personCreationStat.setString(4, registrationInfo.getMiddleName());
-            personCreationStat.setString(5, registrationInfo.getLastName());
-            personCreationStat.setObject(6, registrationInfo.getBirthday());
-            personCreationStat.setString(7, registrationInfo.getGender().getGenderName());
-            statements.add(personCreationStat);
-            personCreationStat.executeUpdate();
 
-            int personId = AppConstant.AUTH_USER_STANDARD_INT_VALUE;
-            ResultSet personsGeneratedKeys = personCreationStat.getGeneratedKeys();
-            while (personsGeneratedKeys.next()) {
-                personId = personsGeneratedKeys.getInt(1);
-            }
-            // TODO replace queries with constants
-            PreparedStatement authUserCreationStat = conn.prepareStatement(
-                    "INSERT INTO hospital.auth_user (login, password, role_id, person_id) VALUES (?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
-            authUserCreationStat.setString(1, registrationInfo.getLogin());
-            authUserCreationStat.setString(2, registrationInfo.getPassword());
-            authUserCreationStat.setInt(3, Role.USER.getRoleId());
-            authUserCreationStat.setInt(4, personId);
-            statements.add(authUserCreationStat);
-            authUserCreationStat.executeUpdate();
+            statements[statIndex] = preparePersonInsertion(regInfo, conn);
+            statements[statIndex].executeUpdate();
+            int personId = receiveGeneratedKeyAfterStatementExecution
+                    (statements[statIndex++]);
 
-            int authUserId = AppConstant.AUTH_USER_STANDARD_INT_VALUE;
-            ResultSet authGeneratedKeys = authUserCreationStat.getGeneratedKeys();
-            while (authGeneratedKeys.next()) {
-                authUserId = authGeneratedKeys.getInt(1);
+            statements[statIndex] = prepareAuthUserInsertion(regInfo, conn,
+                    personId);
+            statements[statIndex++].executeUpdate();
+
+            statements[statIndex] = prepareAuthUserSelectionWhileRegistration
+                    (regInfo, conn);
+            rSet = statements[statIndex].executeQuery();
+            if (rSet.next()) {
+                authUser = compileAuthUser(rSet);
             }
 
             conn.commit();
-            // TODO replace queries with constants
-            PreparedStatement selectAuthUserFromDataBase = conn.prepareStatement("SELECT au.id, p.first_name, p.middle_name, p.last_name, aur.auth_user_role_name, au.person_id, au.staff_id " +
-                    "FROM hospital.auth_user au " +
-                    "         JOIN hospital.persons p ON au.person_id = p.person_id " +
-                    "         JOIN hospital.auth_user_roles aur ON au.role_id = aur.auth_user_role_id " +
-                    "WHERE au.login = ?");
-            selectAuthUserFromDataBase.setString(1, registrationInfo.getLogin());
-            statements.add(selectAuthUserFromDataBase);
-            ResultSet resultSet = selectAuthUserFromDataBase.executeQuery();
-            if (resultSet.next()) {
-                int authId = resultSet.getInt(1);
-                String firstName = resultSet.getString(2);
-                String middleName = resultSet.getString(3);
-                String lastName = resultSet.getString(4);
-                Role role = Role.valueOf(resultSet.getString(5));
-                int userId = resultSet.getInt(6);
-                int staffId = resultSet.getInt(7);
-                return new AuthUser(authId, firstName, middleName, lastName,
-                        role, userId, staffId);
-            }
         } catch (SQLException e) {
-            try {
-                conn.rollback();
-            } catch (SQLException ex) {
-                //TODO log4j
-                ex.printStackTrace();
-            }
+            pool.rollBackTransaction(conn);
             throw new DaoException("An error while saving AuthUser", e);
         } catch (ConnectionPoolException e) {
-            try {
-                conn.rollback();
-            } catch (SQLException ex) {
-                //TODO log4j
-                ex.printStackTrace();
-            }
+            pool.rollBackTransaction(conn);
             throw new DaoException("An error while taking a connection from " +
                     "connection pool during saving AuthUser", e);
         } finally {
-            // TODO add a method with Array-param, which closes an array of Prep-statements
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            for (PreparedStatement statement : statements) {
-                try {
-                    if (statement != null) {
-                        statement.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+            pool.closeConnection(conn, statements, rSet);
         }
-        // TODO ??? throw DaoException
-        return null;
+
+        if (authUser == null) {
+            throw new DaoException(String.format("AuthUser saving met an " +
+                    "impossible execution outcome: registration information: " +
+                    "\"%s\"", regInfo.getPassword()));
+        }
+
+        return authUser;
     }
 
     @Override
@@ -217,5 +159,56 @@ public class DefaultAuthUserDao implements AuthUserDao {
         int staffId = resultSet.getInt(7);
         return new AuthUser(authId, firstName, middleName, lastName,
                 role, userId, staffId);
+    }
+
+    private PreparedStatement preparePersonInsertion(RegistrationInfo
+            registrationInfo, Connection conn) throws SQLException {
+        PreparedStatement personCreationStat;
+        personCreationStat = conn.prepareStatement(
+                SqlStatement.INSERT_PERSON_WHILE_REGISTRATION,
+                Statement.RETURN_GENERATED_KEYS);
+        personCreationStat.setString(1, registrationInfo.getEmail());
+        personCreationStat.setString(2, registrationInfo.getPhoneNumber());
+        personCreationStat.setString(3, registrationInfo.getFirstName());
+        personCreationStat.setString(4, registrationInfo.getMiddleName());
+        personCreationStat.setString(5, registrationInfo.getLastName());
+        personCreationStat.setObject(6, registrationInfo.getBirthday());
+        personCreationStat.setString(7, registrationInfo.getGender()
+                .getGenderName());
+        return personCreationStat;
+    }
+
+    private PreparedStatement prepareAuthUserInsertion(RegistrationInfo
+            registrationInfo, Connection conn, int personId)
+            throws SQLException {
+        PreparedStatement authUserCreationStat;
+        authUserCreationStat = conn.prepareStatement(
+                SqlStatement.INSERT_AUTH_USER_WHILE_REGISTRATION,
+                Statement.RETURN_GENERATED_KEYS);
+        authUserCreationStat.setString(1, registrationInfo.getLogin());
+        authUserCreationStat.setString(2, registrationInfo.getPassword());
+        authUserCreationStat.setInt(3, Role.USER.getRoleId());
+        authUserCreationStat.setInt(4, personId);
+        return authUserCreationStat;
+    }
+
+    private PreparedStatement prepareAuthUserSelectionWhileRegistration
+            (RegistrationInfo regInfo, Connection conn) throws SQLException {
+        PreparedStatement selectAuthUser;
+        selectAuthUser = conn.prepareStatement(SqlStatement
+                .SELECT_AUTH_USER_BY_LOGIN);
+        selectAuthUser.setString(1, regInfo.getLogin());
+        return selectAuthUser;
+    }
+
+    private int receiveGeneratedKeyAfterStatementExecution(PreparedStatement
+            personCreationStat) throws SQLException {
+        int personId;
+        personId = AppConstant.AUTH_USER_STANDARD_INT_VALUE;
+        ResultSet personsGeneratedKeys = personCreationStat.getGeneratedKeys();
+        while (personsGeneratedKeys.next()) {
+            personId = personsGeneratedKeys.getInt(1);
+        }
+        return personId;
     }
 }
