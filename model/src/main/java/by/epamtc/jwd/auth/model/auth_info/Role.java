e034cbe77bfb2f0dc0a06f223c19391f35a38bd1
Siarhei Busel <sju.busel@gmail.com>
2020-10-30 01:55:57 +0300

    Refactor Role

--- model/src/main/java/by/epamtc/jwd/auth/model/auth_info/Role.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/auth_info/Role.java
@@ -1,7 +1,8 @@
 package by.epamtc.jwd.auth.model.auth_info;
 
 public enum Role {
-    USER(1);
+    USER(1), REGISTRAR(2), AID(3), NURSE(4), INTERN(5), DOCTOR(6),
+    CHIEF_DEPARTMENT(7), ADMIN(8);
 
     private int roleId;
 
