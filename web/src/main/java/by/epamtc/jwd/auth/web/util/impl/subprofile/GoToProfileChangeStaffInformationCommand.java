1d4df26519bebf0a9975f6793e620bd939873114
Siarhei Busel <sju.busel@gmail.com>
2020-11-01 02:19:45 +0300

    Implement a command "GoToProfileChangeStaffInformationCommand"

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangeStaffInformationCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangeStaffInformationCommand.java
@@ -11,6 +11,6 @@ public class GoToProfileChangeStaffInformationCommand implements Command {
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-
+        req.getRequestDispatcher("/WEB-INF/jsp/subprofile/changeStaffInfo.jsp").forward(req, res);
     }
 }
