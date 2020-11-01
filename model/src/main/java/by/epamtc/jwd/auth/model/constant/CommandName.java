2fb4d88fd07ca276698709d5f9c5166d8189c5a1
Siarhei Busel <sju.busel@gmail.com>
2020-11-01 03:20:09 +0300

    Widen CommandName.java

--- model/src/main/java/by/epamtc/jwd/auth/model/constant/CommandName.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/constant/CommandName.java
@@ -16,6 +16,27 @@ public final class CommandName {
     public static final String GO_TO_PATIENTS = "GO-TO-PATIENTS";
     public static final String GO_TO_STAFF = "GO-TO-STAFF";
 
+    public static final String SUBPROFILE_GO_TO_ALLERGIC_REACTIONS
+            = "GO-TO-PROFILE-ALLERGIC-REACTIONS";
+    public static final String SUBPROFILE_GO_TO_EMAIL_CHANGE
+            = "GO-TO-PROFILE-EMAIL-CHANGE";
+    public static final String SUBPROFILE_GO_TO_CHANGE_PASSWORD
+            = "GO-TO-PROFILE-CHANGE-PASSWORD";
+    public static final String SUBPROFILE_GO_TO_CHANGE_PATIENT_INFO
+            = "GO-TO-PROFILE-CHANGE-PATIENT-INFO";
+    public static final String SUBPROFILE_GO_TO_CHANGE_PHOTO
+            = "GO-TO-PROFILE-CHANGE-PHOTO";
+    public static final String SUBPROFILE_GO_TO_CHANGE_STAFF_INFO
+            = "GO-TO-PROFILE-CHANGE-STAFF-INFO";
+    public static final String SUBPROFILE_GO_TO_CHANGE_STAFF_PHOTO
+            = "GO-TO-PROFILE-CHANGE-STAFF-PHOTO";
+    public static final String SUBPROFILE_GO_TO_EXTREMELY_HAZARDOUS_DISEASES
+            = "GO-TO-PROFILE-EXTREMELY-HAZARDOUS-DISEASES";
+    public static final String SUBPROFILE_GO_TO_STAFF_HISTORY
+            = "GO-TO-PROFILE-STAFF-HISTORY";
+    public static final String SUBPROFILE_GO_TO_STAFF_INFO
+            = "GO-TO-PROFILE-STAFF-INFO";
+
     private CommandName() {
     }
 }
