27e558f7fecfe43cf8dc8d7b271dfcb6026b9031
Siarhei Busel <sju.busel@gmail.com>
2020-11-02 21:38:37 +0300

    Set a request-scoped attribute "patientInfo" in "GoToProfileCommand.java"

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/GoToProfileCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/GoToProfileCommand.java
@@ -1,5 +1,8 @@
 package by.epamtc.jwd.auth.web.util.impl;
 
+import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+import by.epamtc.jwd.auth.model.constant.AppAttribute;
+import by.epamtc.jwd.auth.model.user_info.PatientInfo;
 import by.epamtc.jwd.auth.service.ProfileService;
 import by.epamtc.jwd.auth.service.ServiceFactory;
 import by.epamtc.jwd.auth.web.util.Command;
@@ -17,7 +20,11 @@ public class GoToProfileCommand implements Command {
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        AuthUser currentUser = (AuthUser) req.getSession()
+                .getAttribute(AppAttribute.SESSION_AUTH_USER);
+        PatientInfo patientInfo = profileService.fetchPatientInfo(currentUser);
 
+        req.setAttribute(AppAttribute.REQUEST_PATIENT_INFO, patientInfo);
         req.getRequestDispatcher(CommandPath.PROFILE_JSP).forward(req, res);
     }
 }
