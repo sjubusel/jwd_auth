6bfb3ed6866f8f91d2e26b27d647b20ea87ed308
Siarhei Busel <sju.busel@gmail.com>
2020-11-02 22:10:20 +0300

    Add an AuthUser parameter in DefaultProfileDao.java, ProfileDao.java

--- dao/src/main/java/by/epamtc/jwd/auth/dao/ProfileDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/ProfileDao.java
@@ -1,7 +1,8 @@
 package by.epamtc.jwd.auth.dao;
 
+import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.user_info.PatientInfo;
 
 public interface ProfileDao {
-    PatientInfo fetchPatientInfo();
+    PatientInfo fetchPatientInfo(AuthUser authUser);
 }
--- dao/src/main/java/by/epamtc/jwd/auth/dao/impl/DefaultProfileDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/impl/DefaultProfileDao.java
@@ -2,13 +2,14 @@ package by.epamtc.jwd.auth.dao.impl;
 
 import by.epamtc.jwd.auth.dao.ProfileDao;
 import by.epamtc.jwd.auth.dao.pool.ConnectionPool;
+import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.user_info.PatientInfo;
 
 public class DefaultProfileDao implements ProfileDao {
     private ConnectionPool pool = ConnectionPool.getInstance();
 
     @Override
-    public PatientInfo fetchPatientInfo() {
+    public PatientInfo fetchPatientInfo(AuthUser authUser) {
 
         return null;
     }
