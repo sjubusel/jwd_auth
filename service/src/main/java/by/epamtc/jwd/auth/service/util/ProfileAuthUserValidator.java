38f5a37b320ece28636cbc00fb70ae83447df80b
Siarhei Busel <sju.busel@gmail.com>
2020-11-02 22:12:10 +0300

    Refactor isAuthUserValidToFetchPatientInfo() in "ProfileAuthUserValidator"

--- service/src/main/java/by/epamtc/jwd/auth/service/util/ProfileAuthUserValidator.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/util/ProfileAuthUserValidator.java
@@ -4,6 +4,9 @@ import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 
 public class ProfileAuthUserValidator {
     public boolean isAuthUserValidToFetchPatientInfo(AuthUser authUser) {
-        return authUser.getId() >= 0;
+        if (authUser != null) {
+            return authUser.getId() >= 0;
+        }
+        return false;
     }
 }
