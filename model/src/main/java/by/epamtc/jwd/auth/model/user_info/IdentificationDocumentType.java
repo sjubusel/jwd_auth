32f80be45278c56e0c7fbaa15c2fe142fa9386f7
Siarhei Busel <sju.busel@gmail.com>
2020-11-02 23:25:12 +0300

    Refactor a n enum "IdentificationDocumentType.java": add String description

--- model/src/main/java/by/epamtc/jwd/auth/model/user_info/IdentificationDocumentType.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/user_info/IdentificationDocumentType.java
@@ -4,6 +4,7 @@ public enum IdentificationDocumentType {
     PASSPORT(1), BELARUS_RESIDENCE_VISA(2), REFUGEE_IDENTITY_CARD(3);
 
     private int typeId;
+    private String description;
 
     IdentificationDocumentType(int typeId) {
         this.typeId = typeId;
@@ -11,5 +12,13 @@ public enum IdentificationDocumentType {
 
     public int getTypeId() {
         return typeId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
