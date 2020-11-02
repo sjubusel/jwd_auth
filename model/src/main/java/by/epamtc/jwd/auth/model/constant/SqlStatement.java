13e13e5395c309a5ddfbf1a302b8b1da04b54754
Siarhei Busel <sju.busel@gmail.com>
2020-11-02 23:14:50 +0300

    Refactor "SqlStatement.java": add "SELECT_PATIENT_INFO"

--- model/src/main/java/by/epamtc/jwd/auth/model/constant/SqlStatement.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/constant/SqlStatement.java
@@ -16,6 +16,35 @@ public final class SqlStatement {
     public static final String INSERT_AUTH_USER_WHILE_REGISTRATION =
             "INSERT INTO hospital.auth_user (login, password, role_id, " +
                     "person_id) VALUES (?, ?, ?, ?)";
+    public static final String SELECT_PATIENT_INFO = "SELECT p.photo_path, p.first_name, p.middle_name, p.last_name, p.birth_date, p.gender, p.email, p.phone_number,\n" +
+            "        p.marital_status,\n" +
+            "        id.identification_document_id, id.identification_document_type_id, idt.name, id.series,\n" +
+            "        id.serial_number_of_document, id.latin_holder_name, id.latin_holder_surname, c.full_country_name,\n" +
+            "        id.birth_date, id.personal_number, id.gender, id.place_of_origin, id.date_of_issue, id.date_of_expiry,\n" +
+            "        id.issue_authority,\n" +
+            "        a.address_id, a.zip_code, c2.short_country_name, ret.region_type_name, re.region_name,\n" +
+            "        art.area_type_name, ar.area_name, st.settlement_type_name, s.settlement_name, rt.road_type_name, r.road_name,\n" +
+            "        a.house, a.building, a.room,\n" +
+            "        p.in_case_of_emergency_person_id, p.in_case_of_emergency_phone_number, p.blood_type, p.rhesus_factor,\n" +
+            "        p.disability_degree, p.transportation_status, p.allergic_anamnesis_food, p.allergic_anamnesis_medicine,\n" +
+            "        p.extremely_hazardous_diseases\n" +
+            "FROM hospital.persons as p\n" +
+            "         LEFT OUTER JOIN hospital.identification_documents id\n" +
+            "              ON p.identification_document_id = id.identification_document_id\n" +
+            "         LEFT OUTER JOIN hospital.identification_document_types idt\n" +
+            "              ON id.identification_document_type_id = idt.identification_document_type_id\n" +
+            "         LEFT OUTER JOIN hospital.countries c ON id.citizenship_id = c.country_id\n" +
+            "         LEFT OUTER JOIN hospital.addresses a ON p.permanent_home_address_id = a.address_id\n" +
+            "         LEFT OUTER JOIN hospital.roads r ON a.road_id = r.road_id\n" +
+            "         LEFT OUTER JOIN hospital.road_types rt ON r.road_type_id = rt.road_type_id\n" +
+            "         LEFT OUTER JOIN hospital.settlements s ON r.settlement_id = s.settlement_id\n" +
+            "         LEFT OUTER JOIN hospital.settlement_types st ON s.settlement_type_id = st.settlement_type_id\n" +
+            "         LEFT OUTER JOIN hospital.areas ar ON s.area_id = ar.area_id\n" +
+            "         LEFT OUTER JOIN hospital.area_types art ON ar.area_type_id = art.area_type_id\n" +
+            "         LEFT OUTER JOIN hospital.regions re ON ar.region_id = re.region_id\n" +
+            "         LEFT OUTER JOIN hospital.region_types ret ON re.region_type_id = ret.region_type_id\n" +
+            "         LEFT OUTER JOIN hospital.countries c2 ON re.country_id = c2.country_id\n" +
+            "WHERE p.person_id = ?;";
 
     private SqlStatement() {
     }
