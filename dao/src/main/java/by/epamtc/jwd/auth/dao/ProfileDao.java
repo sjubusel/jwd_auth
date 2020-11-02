a7cbf4b11a0d3cceb2e7f4ae11b704f4dd6ba079
Siarhei Busel <sju.busel@gmail.com>
2020-11-02 22:14:24 +0300

    Refactor fetchPatientInfo() in "ProfileDao"

--- dao/src/main/java/by/epamtc/jwd/auth/dao/ProfileDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/ProfileDao.java
@@ -1,8 +1,9 @@
 package by.epamtc.jwd.auth.dao;
 
+import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.user_info.PatientInfo;
 
 public interface ProfileDao {
-    PatientInfo fetchPatientInfo(AuthUser authUser);
+    PatientInfo fetchPatientInfo(AuthUser authUser) throws DaoException;
 }
