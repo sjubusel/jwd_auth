1e5b32887aed5ec63769d525ca762d95dda93dfd
Siarhei Busel <sju.busel@gmail.com>
2020-11-03 16:57:15 +0300

    Implement complete fetchPatientInfo() method in "DefaultProfileService"

--- service/src/main/java/by/epamtc/jwd/auth/service/ProfileService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ProfileService.java
@@ -2,7 +2,8 @@ package by.epamtc.jwd.auth.service;
 
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.user_info.PatientInfo;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 
 public interface ProfileService {
-    PatientInfo fetchPatientInfo(AuthUser authUser);
+    PatientInfo fetchPatientInfo(AuthUser authUser) throws ServiceException;
 }
--- service/src/main/java/by/epamtc/jwd/auth/service/impl/DefaultProfileService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/impl/DefaultProfileService.java
@@ -2,9 +2,11 @@ package by.epamtc.jwd.auth.service.impl;
 
 import by.epamtc.jwd.auth.dao.DaoFactory;
 import by.epamtc.jwd.auth.dao.ProfileDao;
+import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.user_info.PatientInfo;
 import by.epamtc.jwd.auth.service.ProfileService;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.service.util.ProfileAuthUserValidator;
 
 
@@ -15,9 +17,14 @@ public class DefaultProfileService implements ProfileService {
             = new ProfileAuthUserValidator();
 
     @Override
-    public PatientInfo fetchPatientInfo(AuthUser authUser) {
+    public PatientInfo fetchPatientInfo(AuthUser authUser)
+            throws ServiceException {
         if (validator.isAuthUserValidToFetchPatientInfo(authUser)) {
-            return profileDao.fetchPatientInfo(authUser);
+            try {
+                return profileDao.fetchPatientInfo(authUser);
+            } catch (DaoException e) {
+                throw new ServiceException(e);
+            }
         }
         return null;
     }
