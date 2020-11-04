91729f4d57e2f0353f5c431f321fb8be507ca431
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 22:38:04 +0300

    Correct a tech error in ProfileAuthUserValidator

--- service/src/main/java/by/epamtc/jwd/auth/service/validation/ProfileAuthUserValidator.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/validation/ProfileAuthUserValidator.java
@@ -5,7 +5,7 @@ import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 public class ProfileAuthUserValidator {
     public boolean isAuthUserValidToFetchPatientInfo(AuthUser authUser) {
         if (authUser != null) {
-            return authUser.getId() >= 0;
+            return authUser.getUserId() > 0;
         }
         return false;
     }
