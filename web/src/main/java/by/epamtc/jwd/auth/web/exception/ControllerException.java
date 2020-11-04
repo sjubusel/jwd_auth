b7c6f42208bd6078b5d45f7591d1c451d7393166
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 15:00:54 +0300

    Add ControllerException on a web-layer

---
+++ web/src/main/java/by/epamtc/jwd/auth/web/exception/ControllerException.java
@@ -0,0 +1,21 @@
+package by.epamtc.jwd.auth.web.exception;
+
+public class ControllerException extends Exception {
+
+    private static final long serialVersionUID = 4578342278296044396L;
+
+    public ControllerException() {
+    }
+
+    public ControllerException(String message) {
+        super(message);
+    }
+
+    public ControllerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ControllerException(Throwable cause) {
+        super(cause);
+    }
+}
