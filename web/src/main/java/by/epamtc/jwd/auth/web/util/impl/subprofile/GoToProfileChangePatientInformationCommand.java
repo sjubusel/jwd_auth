e2e7df598c3ff1b70edeaff457e17bf33165f2f0
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 08:17:30 +0300

    Realize GoToProfileChangePatientInformationCommand.java

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangePatientInformationCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangePatientInformationCommand.java
@@ -1,6 +1,12 @@
 package by.epamtc.jwd.auth.web.util.impl.subprofile;
 
+import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+import by.epamtc.jwd.auth.model.constant.AppAttribute;
 import by.epamtc.jwd.auth.model.constant.CommandPath;
+import by.epamtc.jwd.auth.model.user_info.PatientInfo;
+import by.epamtc.jwd.auth.service.ProfileService;
+import by.epamtc.jwd.auth.service.ServiceFactory;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.util.Command;
 
 import javax.servlet.ServletException;
@@ -9,9 +15,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class GoToProfileChangePatientInformationCommand implements Command {
+    private ServiceFactory factory = ServiceFactory.getInstance();
+    private ProfileService profileService = factory.getProfileService();
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        AuthUser currentUser = (AuthUser) req.getSession()
+                .getAttribute(AppAttribute.SESSION_AUTH_USER);
+        PatientInfo patientInfo = null;
+
+        try {
+            patientInfo = profileService.fetchPatientInfo(currentUser);
+        } catch (ServiceException e) {
+            // TODO log4j in catch of ServiceException
+            req.setAttribute(AppAttribute.REQUEST_ERROR, AppAttribute
+                    .REQUEST_ERROR_VALUE_TECH);
+        }
+
+        if (patientInfo == null) {
+            req.setAttribute(AppAttribute.REQUEST_ERROR, AppAttribute
+                    .REQUEST_ERROR_VALUE_VAL);
+        }
+
         req.getRequestDispatcher(CommandPath.SUBPROFILE_CHANGE_PATIENT_INFO_JSP)
                 .forward(req, res);
     }
