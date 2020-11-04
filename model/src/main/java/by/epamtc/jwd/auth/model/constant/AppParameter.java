99c3b5c7c3a25f1de0c466657dda96ea9ad11ab6
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 15:08:38 +0300

    Widen AppParameter

--- model/src/main/java/by/epamtc/jwd/auth/model/constant/AppParameter.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/constant/AppParameter.java
@@ -18,6 +18,9 @@ public final class AppParameter {
     public static final String BIRTHDAY = "birthday";
     public static final String GENDER = "gender";
 
+    public static final String HEADER_CONTENT_DISPOSITION = "content-disposition";
+    public static final String HEADER_CONTENT_DISPOSITION_FILENAME = "filename";
+
     private AppParameter() {
     }
 }
