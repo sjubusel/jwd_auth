0f541a41e0312b8d0c9aae8c54fc7495de146ff9
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 20:37:12 +0300

    Add TODO in ControllerException.java

--- web/src/main/java/by/epamtc/jwd/auth/web/exception/ControllerException.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/exception/ControllerException.java
@@ -1,5 +1,6 @@
 package by.epamtc.jwd.auth.web.exception;
 
+// TODO ??? DELETE
 public class ControllerException extends Exception {
 
     private static final long serialVersionUID = 4578342278296044396L;
