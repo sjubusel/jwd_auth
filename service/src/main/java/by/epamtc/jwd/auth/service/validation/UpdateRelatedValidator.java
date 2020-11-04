bc40d263fa7a7ceffc3a5118ca78848453845a60
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 22:40:17 +0300

    Add UpdateRelatedValidator

---
+++ service/src/main/java/by/epamtc/jwd/auth/service/validation/UpdateRelatedValidator.java
@@ -0,0 +1,16 @@
+package by.epamtc.jwd.auth.service.validation;
+
+import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+
+import java.io.InputStream;
+
+public class UpdateRelatedValidator {
+
+    public boolean isInitDataValidToUpdate(String targetFileName,
+            InputStream iFileStreamFromClient, AuthUser user) {
+        if (user == null || user.getUserId() > 0) {
+            return false;
+        }
+        return (targetFileName != null) && (iFileStreamFromClient != null);
+    }
+}
