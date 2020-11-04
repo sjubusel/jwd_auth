09ad0463df673f91f8aea7454fce493c2db03d81
Siarhei Busel <sju.busel@gmail.com>
2020-11-04 19:29:48 +0300

    Widen "DefaultUploadService.java" with ServiceException handling

--- service/src/main/java/by/epamtc/jwd/auth/service/UploadService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/UploadService.java
@@ -1,7 +1,9 @@
 package by.epamtc.jwd.auth.service;
 
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 
 public interface UploadService {
-    boolean updatePatientPhoto(String targetFileName, AuthUser user);
+    boolean updatePatientPhoto(String targetFileName, AuthUser user)
+            throws ServiceException;
 }
--- service/src/main/java/by/epamtc/jwd/auth/service/impl/DefaultUploadService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/impl/DefaultUploadService.java
@@ -1,12 +1,25 @@
 package by.epamtc.jwd.auth.service.impl;
 
+import by.epamtc.jwd.auth.dao.DaoFactory;
+import by.epamtc.jwd.auth.dao.UpdateDao;
+import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.service.UploadService;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 
 public class DefaultUploadService implements UploadService {
-    @Override
-    public boolean updatePatientPhoto(String targetFileName, AuthUser user) {
+    private DaoFactory daoFactory = DaoFactory.getInstance();
+    private UpdateDao updateDao = daoFactory.getUpdateDao();
 
-        return false;
+    @Override
+    public boolean updatePatientPhoto(String targetFileName, AuthUser user)
+            throws ServiceException {
+        // validator if targetfilename exists and has Correct naming
+        // validator if user contains all necessary info in upload
+        try {
+            return updateDao.updatePatientPhoto(targetFileName, user);
+        } catch (DaoException e) {
+            throw new ServiceException(e);
+        }
     }
 }
