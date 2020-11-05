6c1665579eedd7f5c290373aff5124ccf45afb8e
Siarhei Busel <sju.busel@gmail.com>
2020-11-05 22:04:11 +0300

    Widen MaritalStatus with enum-values

--- model/src/main/java/by/epamtc/jwd/auth/model/user_info/MaritalStatus.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/user_info/MaritalStatus.java
@@ -1,13 +1,19 @@
 package by.epamtc.jwd.auth.model.user_info;
 
 public enum MaritalStatus {
-    UNMARRIED("холост / незамужем"), MARRIED("женат / замужем"),
-    DIVORCED("разведён / разведена"), WIDOWER("вдовец / вдова");
+    UNMARRIED("unmarried", "холост / незамужем"), MARRIED("married", "женат / замужем"),
+    DIVORCED("divorced", "разведён / разведена"), WIDOWER("widower", "вдовец / вдова");
 
+    private String value;
     private String description;
 
-    MaritalStatus(String description) {
+    MaritalStatus(String value, String description) {
+        this.value = value;
         this.description = description;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     public String getDescription() {
