7ff8cd7c53363f83ab3618fb7799ebec9153d9ce
Siarhei Busel <sju.busel@gmail.com>
2020-11-05 22:23:23 +0300

    Widen CommandProvider with new get commands

--- web/src/main/java/by/epamtc/jwd/auth/web/util/CommandProvider.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/CommandProvider.java
@@ -54,8 +54,8 @@ public class CommandProvider {
         repository.put(CommandName.GO_TO_PATIENTS, new GoToPatientsCommand());
         repository.put(CommandName.GO_TO_STAFF, new GoToStaffCommand());
 
-//        repository.put(CommandName.SUBPROFILE_GO_TO_ALLERGIC_REACTIONS,
-//                new GoToProfileAllergicReactionsCommand());
+        repository.put(CommandName.SUBPROFILE_GO_TO_ALLERGIC_REACTIONS,
+                new GoToProfileAllergicReactionsCommand());
         repository.put(CommandName.SUBPROFILE_GO_TO_EMAIL_CHANGE,
                 new GoToProfileChangeEmailCommand());
 //        repository.put(CommandName.SUBPROFILE_GO_TO_CHANGE_PASSWORD,
@@ -70,8 +70,8 @@ public class CommandProvider {
 //                new GoToProfileChangeStaffInformationCommand());
 //        repository.put(CommandName.SUBPROFILE_GO_TO_CHANGE_STAFF_PHOTO,
 //                new GoToProfileChangeStaffPhotoCommand());
-//        repository.put(CommandName.SUBPROFILE_GO_TO_EXTREMELY_HAZARDOUS_DISEASES,
-//                new GoToProfileExtremelyHazardousDiseasesCommand());
+        repository.put(CommandName.SUBPROFILE_GO_TO_EXTREMELY_HAZARDOUS_DISEASES,
+                new GoToProfileExtremelyHazardousDiseasesCommand());
 //        repository.put(CommandName.SUBPROFILE_GO_TO_STAFF_HISTORY,
 //                new GoToProfileStaffHistoryCommand());
 //        repository.put(CommandName.SUBPROFILE_GO_TO_STAFF_INFO,
