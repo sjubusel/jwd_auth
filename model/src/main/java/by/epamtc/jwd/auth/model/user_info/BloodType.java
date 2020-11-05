002907116696cf53ec527de0f4ffe6941ba20b71
Siarhei Busel <sju.busel@gmail.com>
2020-11-05 23:04:16 +0300

    Rollback BloodType.java

--- model/src/main/java/by/epamtc/jwd/auth/model/user_info/BloodType.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/user_info/BloodType.java
@@ -1,19 +1,12 @@
 package by.epamtc.jwd.auth.model.user_info;
 
 public enum BloodType {
-    FIRST(1, "I"), SECOND(2, "II"), THIRD(3, "III"), FORTH(4, "IV");
+    FIRST("I"), SECOND("II"), THIRD("III"), FORTH("IV");
 
-    private int number;
     private String value;
 
-    BloodType(int number, String value) {
-        this.number = number;
+    BloodType(String value) {
         this.value = value;
-    }
-
-
-    public int getNumber() {
-        return number;
     }
 
     public String getValue() {
