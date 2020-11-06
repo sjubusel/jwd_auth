c0b0592bc537ed3e33bb2e8c7090264513e0c6c5
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 22:48:41 +0300

    Widen stub FetchCountryInChangePatientInfoJspAjaxCommand.java

--- web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchCountryInChangePatientInfoJspAjaxCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchCountryInChangePatientInfoJspAjaxCommand.java
@@ -1,5 +1,7 @@
 package by.epamtc.jwd.auth.web.ajax.impl;
 
+import by.epamtc.jwd.auth.service.ajax.AjaxFetchService;
+import by.epamtc.jwd.auth.service.ajax.AjaxServiceFactory;
 import by.epamtc.jwd.auth.web.ajax.AjaxCommand;
 
 import javax.servlet.ServletException;
@@ -8,6 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FetchCountryInChangePatientInfoJspAjaxCommand implements AjaxCommand {
+    private AjaxServiceFactory ajaxServiceFactory = AjaxServiceFactory.getInstance();
+    private AjaxFetchService ajaxFetchService = ajaxServiceFactory
+            .getAjaxFetchService();
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
