1826a489b583b514d38286cc1f05dea5871ee253
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 22:52:27 +0300

    Widen AjaxFetchService.java

--- service/src/main/java/by/epamtc/jwd/auth/service/ajax/AjaxFetchService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ajax/AjaxFetchService.java
@@ -1,4 +1,7 @@
 package by.epamtc.jwd.auth.service.ajax;
 
+import java.util.List;
+
 public interface AjaxFetchService {
+    List<String> fetchCountries(String countryElement);
 }
--- service/src/main/java/by/epamtc/jwd/auth/service/ajax/impl/DefaultAjaxFetchService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ajax/impl/DefaultAjaxFetchService.java
@@ -1,6 +1,16 @@
 package by.epamtc.jwd.auth.service.ajax.impl;
 
 import by.epamtc.jwd.auth.service.ajax.AjaxFetchService;
+import by.epamtc.jwd.auth.service.ajax.validation.AjaxValidator;
+
+import java.util.List;
 
 public class DefaultAjaxFetchService implements AjaxFetchService {
+    private AjaxValidator ajaxValidator = new AjaxValidator();
+
+
+    @Override
+    public List<String> fetchCountries(String countryElement) {
+        return null;
+    }
 }
