a3021f2693e91e2116804da6864b6e2d1960c8be
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 23:02:44 +0300

    Widen {DefaultAjaxFetchDao.java} with {ConnectionPool}

--- dao/src/main/java/by/epamtc/jwd/auth/dao/ajax/impl/DefaultAjaxFetchDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/ajax/impl/DefaultAjaxFetchDao.java
@@ -1,10 +1,13 @@
 package by.epamtc.jwd.auth.dao.ajax.impl;
 
 import by.epamtc.jwd.auth.dao.ajax.AjaxFetchDao;
+import by.epamtc.jwd.auth.dao.pool.ConnectionPool;
 
 import java.util.List;
 
 public class DefaultAjaxFetchDao implements AjaxFetchDao {
+    private ConnectionPool pool = ConnectionPool.getInstance();
+
     @Override
     public List<String> fetchCountries(String countryPart) {
 
