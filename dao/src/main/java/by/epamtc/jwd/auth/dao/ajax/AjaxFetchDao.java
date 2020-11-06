3b03b9e2fadff2815948fa40b192354ff5a1b3b0
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 23:21:51 +0300

    Widen {AjaxFetchDao.java} with a throws block

--- dao/src/main/java/by/epamtc/jwd/auth/dao/ajax/AjaxFetchDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/ajax/AjaxFetchDao.java
@@ -1,7 +1,9 @@
 package by.epamtc.jwd.auth.dao.ajax;
 
+import by.epamtc.jwd.auth.dao.exception.DaoException;
+
 import java.util.List;
 
 public interface AjaxFetchDao {
-    List<String> fetchCountries(String countryPart);
+    List<String> fetchCountries(String countryPart) throws DaoException;
 }
