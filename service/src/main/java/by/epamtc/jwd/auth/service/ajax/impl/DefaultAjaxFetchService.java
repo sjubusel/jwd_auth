db1b19e97067676c03a42b2baf755d9787e48c9b
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 23:01:30 +0300

    Widen DefaultAjaxFetchService.java with dao

--- service/src/main/java/by/epamtc/jwd/auth/service/ajax/impl/DefaultAjaxFetchService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ajax/impl/DefaultAjaxFetchService.java
@@ -1,6 +1,7 @@
 package by.epamtc.jwd.auth.service.ajax.impl;
 
 import by.epamtc.jwd.auth.dao.ajax.AjaxDaoFactory;
+import by.epamtc.jwd.auth.dao.ajax.AjaxFetchDao;
 import by.epamtc.jwd.auth.service.ajax.AjaxFetchService;
 import by.epamtc.jwd.auth.service.ajax.validation.AjaxValidator;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 public class DefaultAjaxFetchService implements AjaxFetchService {
     private AjaxDaoFactory ajaxDaoFactory = AjaxDaoFactory.getInstance();
+    private AjaxFetchDao ajaxFetchDao = ajaxDaoFactory.getAjaxFetchDao();
     private AjaxValidator ajaxValidator = new AjaxValidator();
 
 
