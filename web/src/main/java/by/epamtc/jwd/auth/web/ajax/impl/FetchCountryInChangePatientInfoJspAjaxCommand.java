3a3539e31686ac8c6182e2add3bb57fbd0ae23ce
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 23:10:55 +0300

    Implement {FetchCountryInChangePatientInfoJspAjaxCommand.java}

--- web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchCountryInChangePatientInfoJspAjaxCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchCountryInChangePatientInfoJspAjaxCommand.java
@@ -3,11 +3,14 @@ package by.epamtc.jwd.auth.web.ajax.impl;
 import by.epamtc.jwd.auth.service.ajax.AjaxFetchService;
 import by.epamtc.jwd.auth.service.ajax.AjaxServiceFactory;
 import by.epamtc.jwd.auth.web.ajax.AjaxCommand;
+import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class FetchCountryInChangePatientInfoJspAjaxCommand implements AjaxCommand {
     private AjaxServiceFactory ajaxServiceFactory = AjaxServiceFactory.getInstance();
@@ -17,6 +20,18 @@ public class FetchCountryInChangePatientInfoJspAjaxCommand implements AjaxComman
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        String countryPart = req.getParameter("countryInput");
+        List<String> countries = ajaxFetchService.fetchCountries(countryPart);
+        write(countries, res);
 
+    }
+
+    //TODO create new class-singleton, which is to write
+    private void write(List<String> countries, HttpServletResponse res)
+            throws IOException {
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        PrintWriter writer = res.getWriter();
+        writer.write(new Gson().toJson(countries));
     }
 }
