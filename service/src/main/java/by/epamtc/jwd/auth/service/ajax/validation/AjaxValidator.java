ac681593923328cc53975ca497b7df5b2d9ad657
Siarhei Busel <sju.busel@gmail.com>
2020-11-06 23:35:48 +0300

    Implement {AjaxValidator.java}

--- service/src/main/java/by/epamtc/jwd/auth/service/ajax/validation/AjaxValidator.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ajax/validation/AjaxValidator.java
@@ -1,8 +1,7 @@
 package by.epamtc.jwd.auth.service.ajax.validation;
 
 public class AjaxValidator {
-    public boolean isFetchInputValid(String fetchInput){
-
-        return false;
+    public boolean isFetchInputValid(String fetchInput) {
+        return fetchInput.matches("[А-Яа-яЁё \\-]{1,255}");
     }
 }
