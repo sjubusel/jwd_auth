c02e2babc37950183a028ba25c7aa3be2672fcd6
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 13:39:32 +0300

    Implement {FetchRoadInChangePatientInfoJspAjaxCommand.java}

--- web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchRoadInChangePatientInfoJspAjaxCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchRoadInChangePatientInfoJspAjaxCommand.java
@@ -1,16 +1,39 @@
 package by.epamtc.jwd.auth.web.ajax.impl;
 
+import by.epamtc.jwd.auth.service.ajax.AjaxFetchService;
+import by.epamtc.jwd.auth.service.ajax.AjaxServiceFactory;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.ajax.AjaxCommand;
+import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class FetchRoadInChangePatientInfoJspAjaxCommand implements AjaxCommand {
+    private AjaxServiceFactory ajaxServiceFactory = AjaxServiceFactory.getInstance();
+    private AjaxFetchService ajaxFetchService = ajaxServiceFactory
+            .getAjaxFetchService();
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        String settlementId = req.getParameter("hiddenSettlementId");
+        String roadInput = req.getParameter("roadInput");
 
+        List<AjaxRoad> roads = null;
+        try {
+            roads = ajaxFetchService.fetchRoads(settlementId, roadInput);
+        } catch (ServiceException e) {
+            // TODO log4j
+        }
+
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        PrintWriter writer = res.getWriter();
+        writer.write(new Gson().toJson(roads));
     }
 }
