c74a3adf4b731432f158e124e8c94824eb20fdbf
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 05:03:50 +0300

    Widen AjaxCommandName.java

--- model/src/main/java/by/epamtc/jwd/auth/model/ajax/AjaxCommandName.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/ajax/AjaxCommandName.java
@@ -6,6 +6,8 @@ public final class AjaxCommandName {
             = "FETCH-COUNTRY-IN-CHANGE-PATIENT-INFO-JSP";
     public static final String FETCH_REGION_IN_CHANGE_PATIENT_INFO_JSP
             = "FETCH-REGION-IN-CHANGE-PATIENT-INFO-JSP";
+    public static final String FETCH_AREA_IN_CHANGE_PATIENT_INFO_JSP
+            = "FETCH-AREA-IN-CHANGE-PATIENT-INFO-JSP";
 
     private AjaxCommandName() {
     }
