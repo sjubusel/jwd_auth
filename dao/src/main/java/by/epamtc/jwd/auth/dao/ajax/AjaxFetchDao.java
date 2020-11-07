32ec0acccb8fae2218d8398aea9690e588c33ff6
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 04:05:29 +0300

    Widen AjaxFetchDao.java with a new method

--- dao/src/main/java/by/epamtc/jwd/auth/dao/ajax/AjaxFetchDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/ajax/AjaxFetchDao.java
@@ -2,9 +2,13 @@ package by.epamtc.jwd.auth.dao.ajax;
 
 import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.ajax.AjaxCountry;
+import by.epamtc.jwd.auth.model.ajax.AjaxRegion;
 
 import java.util.List;
 
 public interface AjaxFetchDao {
     List<AjaxCountry> fetchCountries(String countryPart) throws DaoException;
+
+    List<AjaxRegion> fetchRegions(int countryNumber, String regionInput)
+            throws DaoException;
 }
