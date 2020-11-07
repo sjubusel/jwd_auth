d110c35710fdf7f8c78e633c5431a4de31b72346
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 12:37:13 +0300

    Widen {AjaxCommandName.java} with a "FETCH_SETTLEMENT_IN_CHANGE_PATIENT_INFO_JSP" constant

--- model/src/main/java/by/epamtc/jwd/auth/model/ajax/AjaxCommandName.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/ajax/AjaxCommandName.java
@@ -8,6 +8,8 @@ public final class AjaxCommandName {
             = "FETCH-REGION-IN-CHANGE-PATIENT-INFO-JSP";
     public static final String FETCH_AREA_IN_CHANGE_PATIENT_INFO_JSP
             = "FETCH-AREA-IN-CHANGE-PATIENT-INFO-JSP";
+    public static final String FETCH_SETTLEMENT_IN_CHANGE_PATIENT_INFO_JSP
+            = "FETCH-SETTLEMENT-IN-CHANGE-PATIENT-INFO-JSP";
 
     private AjaxCommandName() {
     }
