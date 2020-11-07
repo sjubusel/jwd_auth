92fe30d606d3851490fd63b07d98e3c4a4781db6
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 03:47:26 +0300

    Widen {AjaxValidator.java}

--- service/src/main/java/by/epamtc/jwd/auth/service/ajax/validation/AjaxValidator.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ajax/validation/AjaxValidator.java
@@ -1,7 +1,20 @@
 package by.epamtc.jwd.auth.service.ajax.validation;
 
+import by.epamtc.jwd.auth.service.exception.ServiceException;
+
 public class AjaxValidator {
     public boolean isFetchInputValid(String fetchInput) {
         return fetchInput.matches("[А-Яа-яЁё \\-]{1,255}");
+    }
+
+    public boolean isInputValidForRegionFetch(String countryId,
+            String regionInput) throws ServiceException {
+        try {
+            Integer.parseInt(countryId);
+        } catch (NumberFormatException e) {
+            throw new ServiceException("Wrong country id was generated " +
+                    "on a client-side by JS");
+        }
+        return regionInput.matches("[А-Яа-яЁё \\-]{1,255}");
     }
 }
