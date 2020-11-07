f1d5c7db5cffdf0ede171d4f3d988da4463fe3a2
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 12:53:45 +0300

    Widen AjaxFetchService.java with fetchSettlements

--- service/src/main/java/by/epamtc/jwd/auth/service/ajax/AjaxFetchService.java
+++ service/src/main/java/by/epamtc/jwd/auth/service/ajax/AjaxFetchService.java
@@ -3,6 +3,7 @@ package by.epamtc.jwd.auth.service.ajax;
 import by.epamtc.jwd.auth.model.ajax.AjaxArea;
 import by.epamtc.jwd.auth.model.ajax.AjaxCountry;
 import by.epamtc.jwd.auth.model.ajax.AjaxRegion;
+import by.epamtc.jwd.auth.model.ajax.AjaxSettlement;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
 
 import java.util.List;
@@ -15,5 +16,8 @@ public interface AjaxFetchService {
             throws ServiceException;
 
     List<AjaxArea> fetchAreas(String regionId, String areaInput)
+            throws ServiceException;
+
+    List<AjaxSettlement> fetchSettlements(String areaId, String settlementInput)
             throws ServiceException;
 }
