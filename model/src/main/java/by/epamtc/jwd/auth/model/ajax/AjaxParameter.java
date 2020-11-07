23a335932c641dd27f540acf94ff0f13ce9f5b1d
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 15:19:19 +0300

    Add "AjaxParameter.java"

---
+++ model/src/main/java/by/epamtc/jwd/auth/model/ajax/AjaxParameter.java
@@ -0,0 +1,11 @@
+package by.epamtc.jwd.auth.model.ajax;
+
+public final class AjaxParameter {
+    public static final String COUNTRY = "countryInput";
+
+    public static final String AJAX_CONTENT_TYPE = "application/json";
+    public static final String AJAX_CHARACTER_ENCODING = "UTF-8";
+
+    private AjaxParameter() {
+    }
+}
