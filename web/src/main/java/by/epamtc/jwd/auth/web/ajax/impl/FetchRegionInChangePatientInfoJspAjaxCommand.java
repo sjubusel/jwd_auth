ccd63aca39b9f9ee2dffb404edcb333c912829c1
Siarhei Busel <sju.busel@gmail.com>
2020-11-07 04:32:10 +0300

    Implement {FetchRegionInChangePatientInfoJspAjaxCommand.java}

--- web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchRegionInChangePatientInfoJspAjaxCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchRegionInChangePatientInfoJspAjaxCommand.java
@@ -1,16 +1,46 @@
 package by.epamtc.jwd.auth.web.ajax.impl;
 
+import by.epamtc.jwd.auth.model.ajax.AjaxRegion;
+import by.epamtc.jwd.auth.service.ajax.AjaxFetchService;
+import by.epamtc.jwd.auth.service.ajax.AjaxServiceFactory;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.ajax.AjaxCommand;
+import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class FetchRegionInChangePatientInfoJspAjaxCommand implements AjaxCommand {
+    private AjaxServiceFactory ajaxServiceFactory = AjaxServiceFactory.getInstance();
+    private AjaxFetchService ajaxFetchService = ajaxServiceFactory
+            .getAjaxFetchService();
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        String countryId = req.getParameter("hiddenCountryId");
+        String regionInput = req.getParameter("regionInput");
 
+        List<AjaxRegion> regions = null;
+        try {
+            regions = ajaxFetchService.fetchRegions(countryId, regionInput);
+        } catch (ServiceException e) {
+            // TODO log4j
+        }
+
+        write(regions, res);
+    }
+
+    //TODO create new class-singleton, which is to write
+    private void write(Object countries, HttpServletResponse res)
+            throws IOException {
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        PrintWriter writer = res.getWriter();
+        writer.write(new Gson().toJson(countries));
     }
 }
