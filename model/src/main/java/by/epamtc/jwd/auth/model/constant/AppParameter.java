fa0a081ddb24ec6091dd0891e72f2683d52a200c
Siarhei Busel <sju.busel@gmail.com>
2020-11-08 16:51:16 +0300

    Widen AppParameter.java

--- model/src/main/java/by/epamtc/jwd/auth/model/constant/AppParameter.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/constant/AppParameter.java
@@ -24,6 +24,45 @@ public final class AppParameter {
     public static final String HEADER_CONTENT_DISPOSITION_FILENAME = "filename";
     public static final String PHOTO_UPLOAD_MESSAGE = "photoUpload";
 
+    public static final String MARITAL_STATUS = "maritalStatusInput";
+    public static final String IN_CASE_OF_EMERGENCY_CONTACT_PERSON_INFO
+            = "inCaseOfEmergencyContactPersonInfoInput";
+    public static final String BLOOD_TYPE = "bloodTypeInput";
+    public static final String RH_BLOOD_GROUP = "rhBloodGroupInput";
+    public static final String DISABILITY_DEGREE = "disabilityDegreeInput";
+    public static final String TRANSPORTATION_STATUS = "transportationStatusInput";
+    public static final String IS_NEW_PHONE = "isNewPhone";
+    public static final String IS_NEW_DOCUMENT = "isNewIdDocument";
+    public static final String ID_DOCUMENT = "idDocumentInput";
+    public static final String SERIES = "seriesInput";
+    public static final String ID_DOCUMENT_NUMBER = "idDocumentNumberInput";
+    public static final String LATIN_NAME = "latinHolderNameInput";
+    public static final String LATIN_SURNAME = "latinHolderSurnameInput";
+    public static final String HIDDEN_CITIZENSHIP = "hiddenCitizenshipInput";
+    public static final String CHANGE_BIRTHDAY = "birthdayInput";
+    public static final String PERSONAL_NUMBER = "personalNumberInput";
+    public static final String PLACE_OF_ORIGIN = "placeOfOriginInput";
+    public static final String DATE_OF_ISSUE = "dateOfIssueInput";
+    public static final String DATE_OF_EXPIRY = "dateOfExpiryInput";
+    public static final String ISSUEING_AUTHORITY = "issueAuthorityInput";
+    public static final String IS_NEW_ADDRESS = "isNewAddress";
+    public static final String ZIP_CODE = "zipCodeInput";
+    public static final String HIDDEN_COUNTRY = "hiddenCountryInput";
+    public static final String HIDDEN_REGION = "hiddenRegionInput";
+    public static final String HIDDEN_AREA = "hiddenAreaInput";
+    public static final String HIDDEN_SETTLEMENT = "hiddenSettlementInput";
+    public static final String HIDDEN_ROAD = "hiddenRoadInput";
+    public static final String HOUSE = "houseInput";
+    public static final String BUILDING = "buildingInput";
+    public static final String ROOM = "roomInput";
+    public static final String IS_NEW_EMERGENCY_PHONE = "isNewEmergencyPhone";
+    public static final String EMERGENCY_NUMBER_COUNTRY_CODE
+            = "emergencyPhoneNumberCountryCode";
+    public static final String EMERGENCY_NUMBER_INNER_CODE
+            = "emergencyPhoneNumberInnerCode";
+    public static final String EMERGENCY_INNER_NUMBER
+            = "emergencyPhoneNumberInnerNumber";
+
     private AppParameter() {
     }
 }
