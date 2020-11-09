505456460263416b7f82ce159d766d03177a169d
Siarhei Busel <sju.busel@gmail.com>
2020-11-09 05:23:21 +0300

    Finish {GoToProfileChangeEmailCommand.java}

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangeEmailCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangeEmailCommand.java
@@ -1,5 +1,6 @@
 package by.epamtc.jwd.auth.web.util.impl.subprofile;
 
+import by.epamtc.jwd.auth.model.constant.AppParameter;
 import by.epamtc.jwd.auth.model.constant.CommandPath;
 import by.epamtc.jwd.auth.web.util.Command;
 
@@ -12,6 +13,11 @@ public class GoToProfileChangeEmailCommand implements Command {
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        String emailChangeResult = req.getParameter(AppParameter.CHANGE_RESULT);
+        if (emailChangeResult != null) {
+            req.setAttribute(AppParameter.CHANGE_RESULT, emailChangeResult);
+        }
+
         req.getRequestDispatcher(CommandPath.SUBPROFILE_CHANGE_EMAIL_JSP)
                 .forward(req, res);
     }
