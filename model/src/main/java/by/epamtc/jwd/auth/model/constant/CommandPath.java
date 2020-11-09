093b1b2207e31233f914415c2a475e7c25d26497
Siarhei Busel <sju.busel@gmail.com>
2020-11-09 05:23:01 +0300

    Add several CommandPaths into {CommandPath.java}

--- model/src/main/java/by/epamtc/jwd/auth/model/constant/CommandPath.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/constant/CommandPath.java
@@ -32,6 +32,18 @@ public final class CommandPath {
     public static final String SUBPROFILE_GO_TO_CHANGE_PATIENT_INFO_CHANGE_RESULT_SUCCESS
             = "/profile?command=go-to-profile-change-patient-info&changeResult=success";
 
+    public static final String SUBPROFILE_GO_TO_EMAIL_CHANGE_RESULT_TECH_ERROR
+            = "/profile?command=go-to-profile-email-change&changeResult=techError";
+    public static final String SUBPROFILE_GO_TO_EMAIL_CHANGE_RESULT_VALID_ERROR
+            = "/profile?command=go-to-profile-email-change&changeResult=validationError";
+    public static final String SUBPROFILE_GO_TO_EMAIL_CHANGE_RESULT_DUPLICATE_ERROR
+            = "/profile?command=go-to-profile-email-change&changeResult=duplicateError";
+    public static final String SUBPROFILE_GO_TO_EMAIL_CHANGE_RESULT_ILLEGAL_PASSWORD_ERROR
+            = "/profile?command=go-to-profile-email-change&changeResult=illegalPassWordError";
+    public static final String SUBPROFILE_GO_TO_EMAIL_CHANGE_SUCCESSFUL_RESULT
+            = "/profile?command=go-to-profile-email-change&changeResult=success";
+
+
     public static final String LOGIN_JSP = "/WEB-INF/jsp/login.jsp";
     public static final String PROFILE_JSP = "/WEB-INF/jsp/profile.jsp";
     public static final String REGISTER_JSP = "/WEB-INF/jsp/register.jsp";
