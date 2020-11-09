5d91d6639d6d7b59b451a3718d0221795b7b43b3
Siarhei Busel <sju.busel@gmail.com>
2020-11-09 14:35:59 +0300

    Implement stub {GoToProfileMedicalHistoryPermissionCommand.java}

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileMedicalHistoryPermissionCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileMedicalHistoryPermissionCommand.java
@@ -1,17 +1,45 @@
 package by.epamtc.jwd.auth.web.util.impl.subprofile;
 
+import by.epamtc.jwd.auth.model.auth_info.AuthUser;
+import by.epamtc.jwd.auth.model.constant.AppAttribute;
 import by.epamtc.jwd.auth.model.constant.CommandPath;
+import by.epamtc.jwd.auth.model.user_info.MedicalHistoryPermission;
+import by.epamtc.jwd.auth.service.ProfileService;
+import by.epamtc.jwd.auth.service.ServiceFactory;
+import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.util.Command;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class GoToProfileMedicalHistoryPermissionCommand implements Command {
+    private ServiceFactory factory = ServiceFactory.getInstance();
+    private ProfileService profileService = factory.getProfileService();
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+        AuthUser user = (AuthUser) req.getSession()
+                .getAttribute(AppAttribute.SESSION_AUTH_USER);
+        List<MedicalHistoryPermission> permissions = null;
+        try {
+            permissions = profileService.fetchMedicalHistoryPermissions(user);
+        } catch (ServiceException e) {
+            // TODO log4j
+            req.setAttribute(AppAttribute.REQUEST_ERROR, AppAttribute
+                    .REQUEST_ERROR_VALUE_TECH);
+        }
+
+        if (permissions == null) {
+            req.setAttribute(AppAttribute.REQUEST_ERROR, AppAttribute
+                    .REQUEST_ERROR_VALUE_VAL);
+        }
+
+
+
         req.getRequestDispatcher(CommandPath
                 .SUBPROFILE_MEDICAL_HISTORY_PERMISSION_jsp).forward(req, res);
     }
