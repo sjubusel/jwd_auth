d060e744f2b93a6809c8efc7162b83046cc34d4c
Siarhei Busel <sju.busel@gmail.com>
2020-11-09 12:37:17 +0300

    Complete an implementation of {changePassword.jsp}

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangePasswordCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/subprofile/GoToProfileChangePasswordCommand.java
@@ -1,5 +1,6 @@
 package by.epamtc.jwd.auth.web.util.impl.subprofile;
 
+import by.epamtc.jwd.auth.model.constant.AppParameter;
 import by.epamtc.jwd.auth.model.constant.CommandPath;
 import by.epamtc.jwd.auth.web.util.Command;
 
@@ -10,7 +11,13 @@ import java.io.IOException;
 
 public class GoToProfileChangePasswordCommand implements Command {
     @Override
-    public void execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public void execute(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        String passwordChangeResult = req.getParameter(AppParameter.CHANGE_RESULT);
+        if (passwordChangeResult != null) {
+            req.setAttribute(AppParameter.CHANGE_RESULT, passwordChangeResult);
+        }
+
         req.getRequestDispatcher(CommandPath.SUBPROFILE_CHANGE_PASSWORD_JSP)
                 .forward(req, res);
     }
