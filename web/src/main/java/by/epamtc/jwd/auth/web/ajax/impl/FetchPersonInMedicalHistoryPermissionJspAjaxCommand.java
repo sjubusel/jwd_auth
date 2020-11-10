e1a3b589a2745b387a1ad6fd9b3e1945b68ca530
Siarhei Busel <sju.busel@gmail.com>
2020-11-10 12:45:37 +0300

    Add exception log hangling in FetchPersonInMedicalHistoryPermissionJspAjaxCommand.java

--- web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchPersonInMedicalHistoryPermissionJspAjaxCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchPersonInMedicalHistoryPermissionJspAjaxCommand.java
@@ -7,6 +7,8 @@ import by.epamtc.jwd.auth.service.ajax.AjaxServiceFactory;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.ajax.AjaxCommand;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +18,9 @@ import java.io.PrintWriter;
 import java.util.List;
 
 public class FetchPersonInMedicalHistoryPermissionJspAjaxCommand implements AjaxCommand {
+    private static final Logger logger = LoggerFactory.getLogger
+            (FetchPersonInMedicalHistoryPermissionJspAjaxCommand.class);
+
     private AjaxServiceFactory ajaxServiceFactory = AjaxServiceFactory.getInstance();
     private AjaxFetchService ajaxFetchService = ajaxServiceFactory
             .getAjaxFetchService();
@@ -29,7 +34,8 @@ public class FetchPersonInMedicalHistoryPermissionJspAjaxCommand implements Ajax
         try {
             persons = ajaxFetchService.fetchPersons(recipientPerson);
         } catch (ServiceException e) {
-            // TODO log4j
+            logger.error("An error occurred while fetching countries from db \n" +
+                    "persons with param \"{}\"", recipientPerson, e);
         }
 
         res.setContentType(AjaxParameter.AJAX_CONTENT_TYPE);
