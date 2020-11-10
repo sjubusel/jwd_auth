1922e3ce50db2ac543ff6067c3975e2f9bc8871b
Siarhei Busel <sju.busel@gmail.com>
2020-11-10 12:48:36 +0300

    Add exception log hangling in FetchRegionInChangePatientInfoJspAjaxCommand.java

--- web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchRegionInChangePatientInfoJspAjaxCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/ajax/impl/FetchRegionInChangePatientInfoJspAjaxCommand.java
@@ -7,6 +7,8 @@ import by.epamtc.jwd.auth.service.ajax.AjaxServiceFactory;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
 import by.epamtc.jwd.auth.web.ajax.AjaxCommand;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +18,9 @@ import java.io.PrintWriter;
 import java.util.List;
 
 public class FetchRegionInChangePatientInfoJspAjaxCommand implements AjaxCommand {
+    private static final Logger logger = LoggerFactory.getLogger
+            (FetchRegionInChangePatientInfoJspAjaxCommand.class);
+
     private AjaxServiceFactory ajaxServiceFactory = AjaxServiceFactory.getInstance();
     private AjaxFetchService ajaxFetchService = ajaxServiceFactory
             .getAjaxFetchService();
@@ -30,7 +35,9 @@ public class FetchRegionInChangePatientInfoJspAjaxCommand implements AjaxCommand
         try {
             regions = ajaxFetchService.fetchRegions(countryId, regionInput);
         } catch (ServiceException e) {
-            // TODO log4j
+            logger.error("An error occurred while fetching from db regions\n" +
+                    "with these params \"countryId: {}\", \"regionInput: {}\"",
+                    countryId, regionInput, e);
         }
 
         res.setContentType(AjaxParameter.AJAX_CONTENT_TYPE);
