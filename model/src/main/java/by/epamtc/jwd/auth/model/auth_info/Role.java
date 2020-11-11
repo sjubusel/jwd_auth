84742ee073a4ac4ff86d40cfe1f15dabcb2e8bcb
Siarhei Busel <sju.busel@gmail.com>
2020-11-11 21:34:17 +0300

    Widen Roles according to sb changes

--- model/src/main/java/by/epamtc/jwd/auth/model/auth_info/Role.java
+++ model/src/main/java/by/epamtc/jwd/auth/model/auth_info/Role.java
@@ -1,8 +1,18 @@
 package by.epamtc.jwd.auth.model.auth_info;
 
 public enum Role {
-    USER(1), REGISTRAR(2), AID(3), NURSE(4), INTERN(5), DOCTOR(6),
-    CHIEF_DEPARTMENT(7), ADMIN(8);
+    USER(1),
+    REGISTRAR(2),
+    ADMISSION_AID(3),
+    AID(4),
+    ADMISSION_NURSE(5),
+    NURSE(6),
+    INTERN(7),
+    ADMISSION_DOCTOR(8),
+    DOCTOR(9),
+    ADMISSION_CHIEF_DEPARTMENT(10),
+    CHIEF_DEPARTMENT(11),
+    ADMIN(12);
 
     private int roleId;
 
