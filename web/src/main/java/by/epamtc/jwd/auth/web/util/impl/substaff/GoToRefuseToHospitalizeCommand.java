914efd729c5fc9192ffbc40756b49a7fa57682d9
Siarhei Busel <sju.busel@gmail.com>
2020-11-22 01:43:05 +0300

    Widen GoToRefuseToHospitalizeCommand

--- web/src/main/java/by/epamtc/jwd/auth/web/util/impl/substaff/GoToRefuseToHospitalizeCommand.java
+++ web/src/main/java/by/epamtc/jwd/auth/web/util/impl/substaff/GoToRefuseToHospitalizeCommand.java
@@ -4,6 +4,7 @@ import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 import by.epamtc.jwd.auth.model.constant.AppAttribute;
 import by.epamtc.jwd.auth.model.constant.AppParameter;
 import by.epamtc.jwd.auth.model.constant.CommandPath;
+import by.epamtc.jwd.auth.model.visit_info.RefusalMedicineRecommendation;
 import by.epamtc.jwd.auth.service.ServiceFactory;
 import by.epamtc.jwd.auth.service.VisitService;
 import by.epamtc.jwd.auth.service.exception.ServiceException;
@@ -15,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class GoToRefuseToHospitalizeCommand implements Command {
     private static final Logger logger = LoggerFactory.getLogger(
