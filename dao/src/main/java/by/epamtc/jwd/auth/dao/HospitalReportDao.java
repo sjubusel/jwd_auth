3d202e13cd46c5cee720299d1458995e53b068c9
Siarhei Busel <sju.busel@gmail.com>
2020-11-27 22:12:03 +0300

    Add javadoc in HospitalReportDao.java

--- dao/src/main/java/by/epamtc/jwd/auth/dao/HospitalReportDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/HospitalReportDao.java
@@ -3,6 +3,19 @@ package by.epamtc.jwd.auth.dao;
 import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.report.HospitalReport;
 
+/**
+ * an interface which is created to fetch information about current state of
+ * hospital and present it for all users even for unsigned on the main page
+ */
 public interface HospitalReportDao {
+    /**
+     * a method which provide current fillability of the hospital
+     *
+     * @return a bean , which stores information about current fillability
+     * of the hospital
+     * @throws DaoException throws if problems connected to database interaction
+     *                      or taking a connection from ConnectionPool
+     *                      will occur
+     */
     HospitalReport receiveHospitalFillability() throws DaoException;
 }
