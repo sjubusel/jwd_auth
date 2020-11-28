e2c374f0051ebeb7f061be64730c2ea3fe5775a0
Siarhei Busel <sju.busel@gmail.com>
2020-11-28 14:01:13 +0300

    Add javadoc in DbResourceManager.java

--- dao/src/main/java/by/epamtc/jwd/auth/dao/pool/util/DbResourceManager.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/pool/util/DbResourceManager.java
@@ -5,12 +5,37 @@ import by.epamtc.jwd.auth.model.constant.AppConstant;
 
 import java.util.ResourceBundle;
 
-public class DbResourceManager {
+/**
+ * a class which facilitates a process of initialization of ConnectionPool.
+ * It is of a Singleton pattern.
+ *
+ * @see ConnectionPool
+ */
+public final class DbResourceManager {
+    /**
+     * a unique and only instance of this class
+     */
     private static volatile DbResourceManager instance;
 
+    /**
+     * a field which represent the main class, which facilitates a process
+     * of initialization of ConnectionPool
+     */
     private ResourceBundle bundle = ResourceBundle.getBundle(AppConstant
             .DEFAULT_BUNDLE);
 
+    /**
+     * a private constructor of a Singleton pattern
+     */
+    private DbResourceManager() {
+    }
+
+    /**
+     * a method which initialize and provide thread safe instance of a
+     * current type
+     *
+     * @return an unique and only instance of a current class
+     */
     public static DbResourceManager getInstance() {
         DbResourceManager localInstance = instance;
         if (localInstance == null) {
