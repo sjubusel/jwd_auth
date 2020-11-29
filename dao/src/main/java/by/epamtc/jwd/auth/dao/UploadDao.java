ccfd8f2aa40b1663cc64e0c1d36fbc1a43f5b4b0
Siarhei Busel <sju.busel@gmail.com>
2020-11-29 10:15:08 +0300

    Add javadoc in UploadDao.java

--- dao/src/main/java/by/epamtc/jwd/auth/dao/UploadDao.java
+++ dao/src/main/java/by/epamtc/jwd/auth/dao/UploadDao.java
@@ -3,6 +3,24 @@ package by.epamtc.jwd.auth.dao;
 import by.epamtc.jwd.auth.dao.exception.DaoException;
 import by.epamtc.jwd.auth.model.auth_info.AuthUser;
 
+/**
+ * a interface which establishes method that will be responsible for
+ * uploading any compatible information to the source
+ */
 public interface UploadDao {
-    boolean updatePatientPhoto(String targetFileName, AuthUser user) throws DaoException;
+    /**
+     * a method which saves to a database information about an uploaded photo
+     * of a patient
+     *
+     * @param targetFileName a name of file with appended relative path to its
+     *                       folder, which can be executed by a Tomcat Server
+     * @param user           an AuthUser object which identifies a person
+     *                       whom photo is uploaded
+     * @return true if changes about uploaded photo is added to the source
+     * @throws DaoException throws if problems connected to database interaction
+     *                      or taking a connection from ConnectionPool
+     *                      will occur
+     */
+    boolean updatePatientPhoto(String targetFileName, AuthUser user)
+            throws DaoException;
 }
