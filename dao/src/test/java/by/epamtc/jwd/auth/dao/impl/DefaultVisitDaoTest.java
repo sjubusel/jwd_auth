9760b4bf374dd6d844d54e28b5026a0185157f62
Siarhei Busel <sju.busel@gmail.com>
2020-11-30 15:50:55 +0300

    Replace asterisk-import(*) with multiple imports in  DefaultVisitDaoTest.java

--- dao/src/test/java/by/epamtc/jwd/auth/dao/impl/DefaultVisitDaoTest.java
+++ dao/src/test/java/by/epamtc/jwd/auth/dao/impl/DefaultVisitDaoTest.java
@@ -15,7 +15,11 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import static org.junit.jupiter.api.Assertions.fail;
